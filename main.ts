99f88d0c7b051317911eaa34c8c7e93e56f9b9be
jakubdrewniak <drewniak.jakub@@gmail.com>
2023-10-02 22:41:10 +0200

    Iterate over apartments.

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { program } from "commander";
 import { webkit, Browser, Page } from "playwright";
 import { expect } from "@playwright/test";
 import * as fs from "fs";
-import cheerio from "cheerio";
+import cheerio, { CheerioAPI } from "cheerio";
 
 type Apartment = { apNumber: number; KW: string };
 let page: Page;
@@ -17,59 +17,71 @@ const SEARCH_URL =
   const browser: Browser = await webkit.launch({ headless, slowMo: 50 });
   page = await browser.newPage();
 
+  console.log('Creating apartments list...')
   const apartments: Apartment[] = await getApartments(
     departmentCode,
     KWNumber,
     controlNumber,
   );
-  console.log(apartments);
-  await prepareApartmentView(apartments[0]);
+  console.log(`Apartments list complete. Length: ${apartments.length}`)
+
+  const templateHtml = fs.readFileSync("template.html", "utf-8");
+  const $ = cheerio.load(templateHtml);
+
+  for (const ap of apartments) {
+    await prepareApartmentView(ap, $);
+  }
+  console.log(`All apartments complete. Creating output file.`)
+  const outputFileName = `output.html`;
+  fs.writeFileSync(outputFileName, $.html());
+  console.log(`Output file created.`)
 
   await browser.close();
 })();
 
-async function prepareApartmentView(ap: Apartment) {
-  const templateHtml = fs.readFileSync("template.html", "utf-8");
-  const $ = cheerio.load(templateHtml);
-
+async function prepareApartmentView(ap: Apartment, $: CheerioAPI) {
+  console.log(`Creating view for apartment ${ap.apNumber}...`)
   const [departmentCode, KWNumber, controlNumber] = ap.KW.split("/").map(
     (val) => val.trim(),
   );
-  await page.goto(SEARCH_URL);
-  await page.locator("#kodWydzialuInput").fill(departmentCode);
-  await page.locator("#numerKsiegiWieczystej").fill(KWNumber);
-  await page.locator("#cyfraKontrolna").fill(controlNumber);
+  try {
+    console.log(`Opening  apartment ${ap.apNumber} KW...`)
+    await page.goto(SEARCH_URL);
+    await page.locator("#kodWydzialuInput").fill(departmentCode);
+    await page.locator("#numerKsiegiWieczystej").fill(KWNumber);
+    await page.locator("#cyfraKontrolna").fill(controlNumber);
 
-  await page.locator("#wyszukaj").click();
+    await page.locator("#wyszukaj").click();
 
-  await page.locator("#przyciskWydrukZwykly").click();
+    await page.locator("#przyciskWydrukZwykly").click();
 
-  await page.locator('input[value="Dział I-Sp"]').click();
+    await page.locator('input[value="Dział I-Sp"]').click();
 
-  $("body").append(`<h2>Mieszkanie ${ap.apNumber}</h3>`);
+    console.log(`Adding apartment number ${ap.apNumber} data...`);
+    $("body").append(`<h2>Mieszkanie ${ap.apNumber}</h3>`);
 
-  const share = await page
-    .locator("tr")
-    .filter({ hasText: "Wielkość udziału w nieruchomości wspólnej" });
-  await expect(share).toBeVisible();
-  const shareElement = await share.evaluate((element) => element.outerHTML);
-  console.log(shareElement);
-  $("body").append(`<table><tbody>${shareElement}</tbody></table>`);
+    const share = await page
+      .locator("tr")
+      .filter({ hasText: "Wielkość udziału w nieruchomości wspólnej" });
+    await expect(share).toBeVisible();
+    const shareElement = await share.evaluate((element) => element.outerHTML);
+    $("body").append(`<table><tbody>${shareElement}</tbody></table>`);
 
-  await page.locator('input[value="Dział II"]').click();
+    await page.locator('input[value="Dział II"]').click();
 
-  const shareHolders = await page
-    .locator("table")
-    .filter({ hasText: "Lista wskazań udziałów w prawie" });
-  await expect(shareHolders).toBeVisible();
-  const shareHoldersElement = await shareHolders.evaluate(
-    (element) => element.outerHTML,
-  );
-  console.log(shareHoldersElement);
-  $("body").append(shareHoldersElement);
-
-  const outputFileName = `output.html`;
-  fs.writeFileSync(outputFileName, $.html());
+    const shareHolders = await page
+      .locator("table")
+      .filter({ hasText: "Lista wskazań udziałów w prawie" });
+    await expect(shareHolders).toBeVisible();
+    const shareHoldersElement = await shareHolders.evaluate(
+      (element) => element.outerHTML,
+    );
+    $("body").append(shareHoldersElement);
+    console.log(`Apartment number ${ap.apNumber} data completed.`)
+  } catch (e) {
+    console.log(`Apartment number ${ap.apNumber} error.`)
+    console.log(e);
+  }
 }
 
 async function getApartments(
@@ -127,6 +139,6 @@ function getOptions() {
   if (!kwSignature) {
     throw new Error("Provide KW signature.");
   }
-  const headless = program.opts().h === "true" || false;
+  const headless = program.opts().h !== "false";
   return { kwSignature, headless };
 }
